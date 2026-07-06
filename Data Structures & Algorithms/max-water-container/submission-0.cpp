8a2d49fafe9b9265574cd640c4c31876554195e3
Udit-code1 <uditpal97601@gmail.com>
2026-07-06 22:48:35 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxArea(vector<int>& h) {
+        int n=h.size();
+        int i=0,j=n-1;
+        int ans=0;
+        while(i<j){
+            int a=min(h[i],h[j])*(j-i);
+            ans=max(ans,a);
+            if(h[i]<h[j])
+                i++;
+            else
+                j--;
+        }
+        return ans;
+    }
+};
