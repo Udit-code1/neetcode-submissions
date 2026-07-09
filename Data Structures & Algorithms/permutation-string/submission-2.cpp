819edeb84e2b7c2ee34b38543f226f22880f488b
Udit-code1 <uditpal97601@gmail.com>
2026-07-09 23:58:54 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        map<char,int>ms1,ms2;
+        for(char c : s1)
+            ms1[c]++;
+        int i=0;
+        for(int j=0;j<s2.size();j++){
+            if(ms1.find(s2[j])==ms1.end()){
+                ms2.clear();
+                i=j+1;
+                continue;
+            }
+            ms2[s2[j]]++;
+            while(ms1[s2[j]]<ms2[s2[j]]){
+                ms2[s2[i]]--;
+                if(ms2[s2[i]]==0)
+                    ms2.erase(s2[i]);
+                i++;
+            }
+            while((j-i+1)>s1.size()){
+                ms2[s2[i]]--;
+                if(ms2[s2[i]]==0)
+                    ms2.erase(s2[i]);
+                i++;
+            }
+
+            if((j-i+1)==s1.size() && ms1==ms2)
+                return true;
+        }
+        return false;
+    }
+};
